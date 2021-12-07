45df00009fc2f4112c6d850539fb645eea166cb0
Eric Baker <eric100596@aol.com>
2021-12-06 19:36:51 -0800

    components

---
+++ components/Nav.js
@@ -0,0 +1,13 @@
+import html from "html-literal";
+
+export default () => html`
+ <nav>
+    <i class="fas fa-bars"></i>
+    <ul class="hidden--mobile nav-links">
+      <li><a href="index.html">Home</a></li>
+      <li><a href="bio.html">Bio</a></li>
+      <li><a href= "gallery.html">Gallery</a></li>
+      <li><a href="register.html">Register</a></li>
+    </ul>
+  </nav>
+  `;
