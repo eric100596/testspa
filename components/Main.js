71f61d10cfd45d853470a4a248e95fda69c3bf12
Eric Baker <eric100596@aol.com>
2021-12-07 07:09:50 -0800

    components/Main.js

--- components/Main.js
+++ components/Main.js
@@ -2,5 +2,6 @@ import html from "html-literal";
 import * as views from "./views";
 
 export default () => html`
-${views["Home"]()}
+  ${views["Home"]()} ${views["Bio"]()} ${views["Gallery"]()}
+  ${views["Register"]()}
 `;
