d94e2876b7bbd6ebbbd03584ca652ee38fc0af63
Eric Baker <eric100596@aol.com>
2021-12-14 21:54:14 -0600

    testspa

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ import axios from "axios";
 import Navigo from "navigo";
 import { capitalize } from "lodash";
 
+import dotenv from "dotenv";
+dotenv.config();
+
 const router = new Navigo(window.location.origin);
 //Move router to the end and add Router.Hooks
 
@@ -26,67 +29,68 @@ function render(st) {
   addEventListeners();
 }
 
-function addEventListeners() {
+//  FUNCTION FOR EVENT LISTENERS
+function addEventListeners(st) {
+  // add event listeners to Nav items for navigation
+  document.querySelectorAll("nav a").forEach(navLink =>
+    navLink.addEventListener("click", event => {
+      event.preventDefault();
+      render(state[event.target.title]);
+    })
+  );
+
   // add menu toggle to bars icon in nav bar
   document
     .querySelector(".fa-bars")
     .addEventListener("click", () =>
       document.querySelector("nav > ul").classList.toggle("hidden--mobile")
     );
+}
 
-  // populating gallery with pictures
-  const gallerySection = document.querySelector("#gallery");
-  // using modules to populate gallery with pictures
-  AddPicturesToGallery(GalleryPictures, gallerySection);
+//Add router here
+router.hooks({
+  before: (done, params) => {
+    const page =
+      params && params.hasOwnProperty("page")
+        ? capitalize(params.page)
+        : "Home";
 
-  // handle form submission with PrintFormOnSubmit module
-  //   const form = document.querySelector("form");
-  //   PrintFormOnSubmit(form);
-  // }
-  //Add router here
-  router.hooks({
-    before: (done, params) => {
-      const page =
-        params && params.hasOwnProperty("page")
-          ? capitalize(params.page)
-          : "Home";
-
-      if (page === "Home") {
-        axios
-          .get(
-            `https://api.openweathermap.org/data/2.5/weather?appid=fbb30b5d6cf8e164ed522e5082b49064&q=st.%20louis`
-          )
-          .then(response => {
-            state.Home.weather = {};
-            state.Home.weather.city = response.data.name;
-            state.Home.weather.temp = response.data.main.temp;
-            state.Home.weather.feelsLike = response.data.main.feels_like;
-            state.Home.weather.description = response.data.weather[0].main;
-            done();
-          })
-          .catch(err => console.log(err));
-      }
-
-      if (page === "Pizza") {
-        axios
-          .get(`${process.env.PIZZA_PLACE_API_URL}`)
-          .then(response => {
-            state.Pizza.pizzas = response.data;
-            done();
-          })
-          .catch(error => {
-            console.log("It puked", error);
-          });
-      }
+    if (page === "Home") {
+      axios
+        .get(
+          `https://api.openweathermap.org/data/2.5/weather?appid=${process.env.OPEN_WEATHER_MAP_API_KEY}&q=st.%20louis`
+        )
+        .then(response => {
+          state.Home.weather = {};
+          state.Home.weather.city = response.data.name;
+          state.Home.weather.temp = response.data.main.temp;
+          state.Home.weather.feelsLike = response.data.main.feels_like;
+          state.Home.weather.description = response.data.weather[0].main;
+          done();
+        })
+        .catch(err => console.log(err));
     }
-  });
 
-  router
-    .on({
-      "/": () => render(state.Home),
-      ":page": params => {
-        let page = capitalize(params.page);
-        render(state[page]);
-      }
-    })
-    .resolve();
+    if (page === "Pizza") {
+      axios
+        .get(`${process.env.PIZZA_PLACE_API_URL}`)
+        .then(response => {
+          state.Pizza.pizzas = response.data;
+          done();
+        })
+        .catch(error => {
+          console.log("It puked", error);
+        });
+    }
+  }
+});
+
+router
+  .on({
+    "/": () => render(state.Home),
+    ":page": params => {
+      let page = capitalize(params.page);
+      render(state[page]);
+    }
+  })
+  .resolve();
